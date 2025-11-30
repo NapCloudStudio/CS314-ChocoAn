ee2a7b2acb745d1b70648f2b02f7a2dd13e34f2e
Enceladus377 <wesleyg@pdx.edu>
2025-11-29 19:17:39 -0800

    Added functionality to manager terminal

--- ChocAn_main.py
+++ ChocAn_main.py
@@ -3,28 +3,48 @@ import provider_terminal
 import generate_reports
 
 def main():
-    print("Welcome to the Chocaholics Anonymous computer system!")
-    print("Please choose an option:")
-    print("1 - Run provider terminal")
-    print("2 - Run manager terminal")
-    print("3 - generate weekly reports")
 
+    do_main_menu = True
+
+    while do_main_menu == True
+        print("Welcome to the Chocaholics Anonymous computer system!")
+        print("Please choose an option:")
+        print("1 - Run provider terminal")
+        print("2 - Run manager terminal")
+        print("3 - generate weekly reports")
+        print("4 - Exit")
+
+        option = get_option(4)
+
+        if option == 1:
+            provider_terminal.provider_terminal()
+        elif option == 2:
+            manager_terminal.manager_terminal()
+        elif option == 3:
+            generate_reports.generate_reports()
+        else:
+            do_main_menu = False
+
+#asks the user to enter a number between 1 and count, and asks them to try again if they enter something else
+def get_option(count):
     while True:
         try:
-            option = int(input("Enter option: "))
-            if 1 <= option <= 3:
+            option = int(input("Enter Option: "))
+            if 1 <= option <= count:
                 break
             else:
-                print("Enter a valid option (1, 2, or 3)")
-        except ValueError:
-            print("Enter a valid option (1, 2, or 3)")
+                print("Enter a valid option (", end = '')
+                for i in range(1, count):
+                    print(f"{i},", end = ' ')
+                print(f"or {count}): ")
 
-    if option == 1:
-        provider_terminal.provider_terminal()
-    elif option == 2:
-        manager_terminal.manager_terminal()
-    else:
-        generate_reports.generate_reports()
+        except ValueError:
+            print("Enter a valid option (", end = '')
+            for i in range(1, count):
+                    print(f"{i},", end = ' ')
+            print(f"or {count}): ")
+    return option
+
 
 
 
--- manager_terminal.py
+++ manager_terminal.py
@@ -1,18 +1,124 @@
+from ChocAn_main import get_option
+from dao import DAO
+
 def manager_terminal():
+    do_manager_terminal = True
 
-    print("Welcome to the Chocaholics Anonymous Manager Terminal!")
-    print("Please chose one of the following options:")
-    print("1 - manage provider list")
-    print("2 - manage service list")
-    print("3 - manage member list")
-    print("4 - exit")
+    while do_manager_terminal == True:
+        print("Welcome to the Chocaholics Anonymous Manager Terminal!")
+        print("Please chose one of the following options:")
+        print("1 - manage provider list")
+        print("2 - manage service list")
+        print("3 - manage member list")
+        print("4 - exit")
 
-    while True:
-        try:
-            option = int(input("Enter option: "))
-            if 1 <= option <= 4:
-                break
-            else:
-                print("Enter a valid option (1, 2, 3, or 4)")
-        except ValueError:
-            print("Enter a valid option (1, 2, 3, or 4)")
+        option = get_option(4)
+
+        if option == 1:
+           manage_providers()
+        elif option == 2:
+            manage_services()
+        elif option == 3:
+            manage_members()
+        else:
+            do_manager_terminal = False
+
+
+def manage_providers():
+    data = DAO("database")
+    data.create_tables()
+    do_manage_providers = True
+
+    while do_manage_providers == True:
+        print("Provider list management:")
+        print("Please chose one of the following options:")
+        print("1 - add provider")
+        print("2 - modify existing provider")
+        print("3 - remove provider")
+        print("4 - return to provider menu")
+
+        option = get_option(4)
+        
+        if option == 1:
+            name = input("Enter the provider's name: ")
+            email = input("Enter the provider's email: ")
+
+            #we might need to implement a hashing algorythm here, though for simplicitys sake we could just storte the passwords in plain text
+            password = input("Enter the provider's password: ")
+            street = input("Enter the provider's streed address: ")
+            city = input("Enter the provider's city: ")
+            state = input("Enter the provider's state: ")
+            zipcode = input("Enter the provider's zip code: ")
+
+            address_id = data.create_address(street, city, state, zip)
+            data.create_provider(name, password, address_id, email, "valid")
+        elif option == 2:
+            print("modify provider")
+            #implement modify function on DAO
+        elif option == 3:
+            print("remove provider")
+            #implement remove function
+        else:
+            do_manage_providers = False
+        
+
+
+def manage_services():
+    data = DAO("database")
+    data.create_tables()
+    do_manage_services = True
+
+    while do_manage_services == True:
+        print("Service list management:")
+        print("Please chose one of the following options:")
+        print("1 - add service")
+        print("2 - remove service")
+        print("3 - return to manager menu")
+
+        option = get_option(3)
+
+        if option == 1:
+            name = input("Enter the service name: ")
+            fee = input("Enter ther service fee: ")
+
+            data.create_service(name, fee)
+        elif option == 2:
+            #add remove service function for DAO
+            print("remove service")
+        else:
+            do_manage_services = False
+
+def manage_members():
+    data = DAO("database")
+    data.create_tables()
+
+    do_manage_members = True
+
+    while do_manage_members == True:
+        print("Member list management:")
+        print("Please chose one of the following options:")
+        print("1 - add member")
+        print("2 - modify existing member")
+        print("3 - remove member")
+        print("4 - return to manager menu")
+
+        option = get_option(4)
+
+        if option == 1:
+            name = input("Enter the member's name: ")
+            street = input("Enter the member's streed address: ")
+            city = input("Enter the member's city: ")
+            state = input("Enter the member's state: ")
+            zipcode = input("Enter the member's zip code: ")
+
+            address_id = data.create_address(street, city, state, zip)
+            data.create_member(name, address_id, "valid")
+
+        elif option == 2:
+            #add modify function to DAO
+            print("modify member")
+        elif option == 3:
+            #add remove function to DAO
+            print("remove member")
+        else:
+            do_manage_members = False
